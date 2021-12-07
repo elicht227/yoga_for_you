827c80fb7f397e4a698333d6eb43eecf92249b1e
Emma Lichtenstein <emma.lichtenstein@kellogg.northwestern.edu>
2021-12-07 18:29:59 +0000

    Whitelist columns in Park for ActiveAdmin

--- app/admin/parks.rb
+++ app/admin/parks.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Park do
 
+ permit_params :park_name, :park_address
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
