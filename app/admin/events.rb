7609162db52370c5204a286cfd625197ec0ccbe0
Emma Lichtenstein <emma.lichtenstein@kellogg.northwestern.edu>
2021-12-07 18:30:03 +0000

    Whitelist columns in Event for ActiveAdmin

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Event do
 
+ permit_params :event_name, :teacher_id, :date, :start_time, :duration, :end_time, :park_id, :category_id, :event_description
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
