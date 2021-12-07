d8fb70971b99551e4cd30b4cdb86322808ef752e
Emma Lichtenstein <emma.lichtenstein@kellogg.northwestern.edu>
2021-12-07 18:31:00 +0000

    Generate graphiti resources for parks

---
+++ app/controllers/api/v1/parks_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::ParksController < Api::V1::GraphitiController
+  def index
+    parks = ParkResource.all(params)
+    respond_with(parks)
+  end
+
+  def show
+    park = ParkResource.find(params)
+    respond_with(park)
+  end
+
+  def create
+    park = ParkResource.build(params)
+
+    if park.save
+      render jsonapi: park, status: 201
+    else
+      render jsonapi_errors: park
+    end
+  end
+
+  def update
+    park = ParkResource.find(params)
+
+    if park.update_attributes
+      render jsonapi: park
+    else
+      render jsonapi_errors: park
+    end
+  end
+
+  def destroy
+    park = ParkResource.find(params)
+
+    if park.destroy
+      render jsonapi: { meta: {} }, status: 200
+    else
+      render jsonapi_errors: park
+    end
+  end
+end
---
+++ app/resources/park_resource.rb
@@ -0,0 +1,12 @@
+class ParkResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :park_name, :string_enum, allow: Park.park_names.keys
+  attribute :park_address, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
