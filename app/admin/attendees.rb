e60cdaab90e803218f2c7ed33a353e681a813e5a
Emma Lichtenstein <emma.lichtenstein@kellogg.northwestern.edu>
2021-12-07 18:30:08 +0000

    Whitelist columns in Attendee for ActiveAdmin

--- app/admin/attendees.rb
+++ app/admin/attendees.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Attendee do
 
+ permit_params :event_id, :student_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
